6bddb8742f79e1c60d80d8b724112bc32b4d7898
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-02 14:10:29 +0300

    feat: parameterize pgBackRest storage module and add VM size and name prefix variables

--- envs/test-azure/terraform/main.tf
+++ envs/test-azure/terraform/main.tf
@@ -17,13 +17,8 @@ module "pgbackrest_storage" {
   source               = "../../../src/terraform/modules/storage"
   resource_group_name  = azurerm_resource_group.rg.name
   location             = azurerm_resource_group.rg.location
-  storage_account_name = "ppgclusterpgbackrest"
-  container_name       = "pgbackrest-repo"
-
-  tags = {
-    AnsibleGroup = "pgbackrest"
-    Environment  = "dev"
-  }
+  storage_account_name = var.storage_account_name
+  container_name       = var.container_name
 }
 
 module "jumpbox" {
@@ -33,18 +28,19 @@ module "jumpbox" {
   subnet_id           = module.network.jumpbox_subnet_id
   subnet_prefix       = var.jumpbox_subnet[0]
 
-  vm_count         = 1
-  vm_name_prefix   = "jumpbox"
-  admin_username   = var.admin_username
-  ssh_public_key   = file(var.ssh_pub_key_path)
   assign_public_ip = true
-  vm_size          = "Standard_D2s_v4"
+
+  vm_count       = 1
+  vm_name_prefix = "jumpbox"
+  admin_username = var.admin_username
+  ssh_public_key = file(var.ssh_pub_key_path)
+  vm_size        = "Standard_D2s_v4"
 
   storage_container_id = module.pgbackrest_storage.storage_container_id
 
   tags = {
     AnsibleGroup = "jumpbox"
-    Environment  = "dev"
+    Environment  = var.environment
   }
 }
 
@@ -55,15 +51,16 @@ module "database_cluster" {
   subnet_id           = module.network.database_subnet_id
   subnet_prefix       = var.database_subnet[0]
 
-  vm_count         = var.vm_count
-  vm_name_prefix   = "percona-node"
-  admin_username   = var.admin_username
-  ssh_public_key   = file(var.ssh_pub_key_path)
   assign_public_ip = false
-  vm_size          = "Standard_D2s_v4"
 
-  create_data_disk  = true
-  data_disk_size_gb = 128
+  vm_count       = var.vm_count
+  vm_name_prefix = var.vm_name_prefix
+  admin_username = var.admin_username
+  ssh_public_key = file(var.ssh_pub_key_path)
+  vm_size        = var.vm_size
+
+  create_data_disk  = var.create_data_disk
+  data_disk_size_gb = var.create_data_disk ? var.data_disk_size_gb : 0
 
   storage_container_id = module.pgbackrest_storage.storage_container_id
 
@@ -71,6 +68,6 @@ module "database_cluster" {
 
   tags = {
     AnsibleGroup = "pg_nodes"
-    Environment  = "dev"
+    Environment  = var.environment
   }
 }
--- envs/test-azure/terraform/variables.tf
+++ envs/test-azure/terraform/variables.tf
@@ -46,6 +46,17 @@ variable "vm_count" {
   default     = 3
 }
 
+variable "vm_size" {
+  description = "Size of the virtual machines (e.g., Standard_D2s_v5)"
+  type        = string
+  default     = "Standard_D2s_v5"
+}
+
+variable "vm_name_prefix" {
+  description = "Prefix for the virtual machine names"
+  type        = string
+}
+
 variable "admin_ip" {
   description = "My public IP address for SSH access to Jumpbox"
   type        = string
@@ -54,5 +65,26 @@ variable "admin_ip" {
 variable "environment" {
   description = "Environment name"
   type        = string
-  default     = "test-azure"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account for pgBackRest repository"
+  type        = string
+}
+
+variable "container_name" {
+  description = "Name of the storage container for pgBackRest repository"
+  type        = string
+}
+
+variable "create_data_disk" {
+  description = "Whether to create an additional disk for the database cluster (true/false)"
+  type        = bool
+  default     = false
+}
+
+variable "data_disk_size_gb" {
+  description = "Size of the additional data disk in GB (applicable if create_data_disk is true)"
+  type        = number
+  default     = 64
 }
