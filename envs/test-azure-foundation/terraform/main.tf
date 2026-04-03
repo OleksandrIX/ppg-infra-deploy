45a2090b5dbd705475d7dbadc2bcdc6e82cbb119
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-03 22:03:32 +0300

    feat: consolidate Terraform configuration by integrating network and storage resources directly into main module

--- envs/test-azure-foundation/terraform/main.tf
+++ envs/test-azure-foundation/terraform/main.tf
@@ -3,26 +3,34 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-module "network" {
-  source               = "../../../src/terraform/modules/network"
-  resource_group_name  = azurerm_resource_group.rg.name
-  location             = azurerm_resource_group.rg.location
-  vnet                 = var.vnet
-  vnet_name            = var.vnet_name
-  database_subnet      = var.database_subnet
-  database_subnet_name = var.database_subnet_name
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.vnet
 }
 
-module "pgbackrest_storage" {
-  source                          = "../../../src/terraform/modules/storage"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location
-  storage_account_name            = var.storage_account_name
-  container_name                  = var.container_name
-  container_access_type           = var.container_access_type
-  account_tier                    = var.account_tier
-  replication_type                = var.replication_type
+resource "azurerm_subnet" "database_subnet" {
+  name                 = var.database_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.database_subnet
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
+
   public_network_access_enabled   = var.public_network_access_enabled
   allow_nested_items_to_be_public = var.allow_nested_items_to_be_public
   min_tls_version                 = var.min_tls_version
+}
+
+resource "azurerm_storage_container" "sc" {
+  name                  = var.container_name
+  storage_account_id    = azurerm_storage_account.sa.id
+  container_access_type = var.container_access_type
 }
--- src/terraform/modules/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
-}
-
-output "database_subnet_id" {
-  value = azurerm_subnet.database_subnet.id
-}
