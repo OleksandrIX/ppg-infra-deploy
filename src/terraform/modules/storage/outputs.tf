42c50d061e339002cbdf39f7b18c70611cbc5dc1
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-03 21:07:18 +0300

    fix: update storage outputs to reference correct storage account and container resources

--- src/terraform/modules/storage/outputs.tf
+++ src/terraform/modules/storage/outputs.tf
@@ -1,20 +1,20 @@
 output "storage_account_name" {
-  value       = azurerm_storage_account.pgbackrest_sa.name
+  value       = azurerm_storage_account.sa.name
   description = "Storage Account name"
 }
 
 output "storage_account_primary_key" {
-  value       = azurerm_storage_account.pgbackrest_sa.primary_access_key
+  value       = azurerm_storage_account.sa.primary_access_key
   sensitive   = true
   description = "Primary access key for the Storage Account"
 }
 
 output "storage_container_name" {
-  value       = azurerm_storage_container.pgbackrest_repo.name
+  value       = azurerm_storage_container.sc.name
   description = "Storage Container name"
 }
 
 output "storage_container_id" {
-  value       = azurerm_storage_container.pgbackrest_repo.id
+  value       = azurerm_storage_container.sc.id
   description = "Storage Container resource ID"
 }
