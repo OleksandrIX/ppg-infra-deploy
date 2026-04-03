80464b02ed47f210f27e74cd611d92f1073b80d5
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-03 14:51:37 +0300

    feat: move compute module to postgres-percona-18 module

---
+++ src/terraform/modules/postgres-percona-18/vms.tf
@@ -0,0 +1,69 @@
+resource "azurerm_network_interface" "nic" {
+  count               = var.vm_count
+  name                = "${var.vm_name_prefix}-nic-${count.index}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = cidrhost(var.subnet_prefix, count.index + 10)
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = var.vm_count
+  name                = "${var.vm_name_prefix}-vm-${count.index}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  tags                = var.tags
+  zone                = tostring((count.index % 3) + 1)
+
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_managed_disk" "data_disk" {
+  count                = var.create_data_disk ? var.vm_count : 0
+  name                 = "${var.vm_name_prefix}-datadisk-${count.index}"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  storage_account_type = "Premium_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = var.data_disk_size_gb
+  zone                 = tostring((count.index % 3) + 1)
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "data_attach" {
+  count              = var.create_data_disk ? var.vm_count : 0
+  managed_disk_id    = azurerm_managed_disk.data_disk[count.index].id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm[count.index].id
+  lun                = 10
+  caching            = "ReadOnly"
+}
