1df38ad14d676d2af3d42aeddb8bc563e1707f24
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-03 18:19:01 +0300

    feat: refactor database cluster module and update variable configurations for improved clarity and functionality

--- envs/test-azure/terraform/main.tf
+++ envs/test-azure/terraform/main.tf
@@ -21,77 +21,18 @@ module "pgbackrest_storage" {
   container_name       = var.container_name
 }
 
-module "jumpbox" {
-  source              = "../../../src/terraform/modules/compute"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = module.network.jumpbox_subnet_id
-  subnet_prefix       = var.jumpbox_subnet[0]
-
-  assign_public_ip = true
-
-  vm_count       = 1
-  vm_name_prefix = "jumpbox"
-  admin_username = var.admin_username
-  ssh_public_key = file(var.ssh_pub_key_path)
-  vm_size        = "Standard_D2s_v4"
-
-  storage_container_id = module.pgbackrest_storage.storage_container_id
-
-  tags = {
-    AnsibleGroup = "jumpbox"
-    Environment  = var.environment
-  }
-}
-
 module "database_cluster" {
-  source              = "../../../src/terraform/modules/compute"
+  source = "../../../src/terraform/modules/postgres-percona-18"
+
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   subnet_id           = module.network.database_subnet_id
   subnet_prefix       = var.database_subnet[0]
+  admin_username      = var.admin_username
+  ssh_public_key      = file(var.ssh_pub_key_path)
 
-  assign_public_ip = false
-
-  vm_count       = var.vm_count
-  vm_name_prefix = var.vm_name_prefix
-  admin_username = var.admin_username
-  ssh_public_key = file(var.ssh_pub_key_path)
-  vm_size        = var.vm_size
-
-  create_data_disk  = var.create_data_disk
-  data_disk_size_gb = var.create_data_disk ? var.data_disk_size_gb : 0
-
-  storage_container_id = module.pgbackrest_storage.storage_container_id
-
-  depends_on = [module.pgbackrest_storage]
-
-  tags = {
-    AnsibleGroup = "pg_nodes"
-    Environment  = var.environment
-  }
-}
-
-module "database_lb" {
-  source                      = "../../../src/terraform/modules/lb"
-  name                        = "percona-postgresql-lb"
-  resource_group_name         = azurerm_resource_group.rg.name
-  location                    = azurerm_resource_group.rg.location
-  backend_vnet_id             = module.network.vnet_id
-  frontend_subnet_id          = module.network.database_subnet_id
-  frontend_private_ip_address = var.database_lb_private_ip
-  probe_interval_in_seconds   = var.database_lb_probe_interval_in_seconds
-  number_of_probes            = var.database_lb_number_of_probes
-
-  backend_ip_addresses = [
-    for vm_name in sort(keys(module.database_cluster.vm_details)) :
-    module.database_cluster.vm_details[vm_name].private_ip
-  ]
-
-  rules = var.database_lb_rules
-
-  tags = {
-    Environment = var.environment
-    Role        = "database-lb"
-  }
+  cluster_vm   = var.cluster_vm
+  ansible_host = var.ansible_host
+  data_disks   = var.data_disks
+  lb           = var.lb
 }
--- envs/test-azure/terraform/variables.tf
+++ envs/test-azure/terraform/variables.tf
@@ -40,23 +40,6 @@ variable "ssh_pub_key_path" {
   default     = "~/.ssh/azure-ppg-cluster.pub"
 }
 
-variable "vm_count" {
-  description = "Number of VMs to create in the database cluster"
-  type        = number
-  default     = 3
-}
-
-variable "vm_size" {
-  description = "Size of the virtual machines (e.g., Standard_D2s_v5)"
-  type        = string
-  default     = "Standard_D2s_v5"
-}
-
-variable "vm_name_prefix" {
-  description = "Prefix for the virtual machine names"
-  type        = string
-}
-
 variable "admin_ip" {
   description = "My public IP address for SSH access to Jumpbox"
   type        = string
@@ -77,62 +60,129 @@ variable "container_name" {
   type        = string
 }
 
-variable "create_data_disk" {
-  description = "Whether to create an additional disk for the database cluster (true/false)"
-  type        = bool
-  default     = false
-}
+variable "cluster_vm" {
+  description = "Cluster VM configuration"
+  type = object({
+    count                         = number
+    name_prefix                   = string
+    size                          = string
+    nic_ip_configuration_name     = string
+    private_ip_address_allocation = string
+    private_ip_host_offset        = number
+    zones                         = list(string)
 
-variable "data_disk_size_gb" {
-  description = "Size of the additional data disk in GB (applicable if create_data_disk is true)"
-  type        = number
-  default     = 64
-}
+    os_disk = object({
+      caching              = string
+      storage_account_type = string
+    })
 
-variable "database_lb_private_ip" {
-  description = "Static private IP address for the internal Azure load balancer"
-  type        = string
-}
-
-variable "database_lb_rules" {
-  description = "Map of internal load balancer rules keyed by logical rule name"
-  type = map(object({
-    frontend_port           = number
-    backend_port            = optional(number)
-    protocol                = optional(string)
-    probe_port              = optional(number)
-    probe_protocol          = optional(string)
-    probe_request_path      = optional(string)
-    idle_timeout_in_minutes = optional(number)
-    disable_outbound_snat   = optional(bool)
-  }))
+    image = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+  })
 
   default = {
-    primary = {
-      frontend_port      = 5000
-      backend_port       = 6432
-      probe_port         = 8008
-      probe_protocol     = "Http"
-      probe_request_path = "/primary"
+    count                         = 3
+    name_prefix                   = "percona-node"
+    size                          = "Standard_D2s_v5"
+    nic_ip_configuration_name     = "internal"
+    private_ip_address_allocation = "Static"
+    private_ip_host_offset        = 10
+    zones                         = ["1", "2", "3"]
+
+    os_disk = {
+      caching              = "ReadWrite"
+      storage_account_type = "Premium_LRS"
     }
-    replica = {
-      frontend_port      = 5001
-      backend_port       = 6432
-      probe_port         = 8008
-      probe_protocol     = "Http"
-      probe_request_path = "/replica"
+
+    image = {
+      publisher = "Canonical"
+      offer     = "0001-com-ubuntu-server-jammy"
+      sku       = "22_04-lts"
+      version   = "latest"
     }
   }
 }
 
-variable "database_lb_probe_interval_in_seconds" {
-  description = "Health probe interval for the internal load balancer"
-  type        = number
-  default     = 5
+variable "ansible_host" {
+  description = "Ansible deployment host configuration"
+  type = object({
+    create                = bool
+    name                  = string
+    vm_size               = string
+    private_ip_hostnumber = number
+  })
+
+  default = {
+    create                = true
+    name                  = "percona-ansible-host"
+    vm_size               = "Standard_B2s"
+    private_ip_hostnumber = 250
+  }
 }
 
-variable "database_lb_number_of_probes" {
-  description = "Number of failed probes before marking a backend unhealthy"
-  type        = number
-  default     = 2
+variable "data_disks" {
+  description = "List of data disks to create per cluster VM"
+  type = list(object({
+    storage_account_type = string
+    create_option        = string
+    size_gb              = number
+    lun                  = number
+    caching              = string
+  }))
+
+  default = []
+}
+
+variable "lb" {
+  description = "Internal load balancer configuration"
+  type = object({
+    name                          = string
+    frontend_private_ip_address   = string
+    frontend_configuration_name   = string
+    sku                           = string
+    private_ip_address_allocation = string
+    probe_interval_in_seconds     = number
+    number_of_probes              = number
+    default_protocol              = string
+    default_idle_timeout_minutes  = number
+    default_disable_outbound_snat = bool
+
+    rules = map(object({
+      frontend_port           = number
+      backend_port            = optional(number)
+      protocol                = optional(string)
+      probe_port              = optional(number)
+      probe_protocol          = optional(string)
+      probe_request_path      = optional(string)
+      idle_timeout_in_minutes = optional(number)
+      disable_outbound_snat   = optional(bool)
+    }))
+  })
+
+  default = {
+    name                          = "ppg-internal-lb"
+    frontend_private_ip_address   = "10.0.0.100"
+    frontend_configuration_name   = "private-frontend"
+    sku                           = "Standard"
+    private_ip_address_allocation = "Static"
+    probe_interval_in_seconds     = 5
+    number_of_probes              = 2
+    default_protocol              = "Tcp"
+    default_idle_timeout_minutes  = 4
+    default_disable_outbound_snat = false
+
+    rules = {
+      postgres = {
+        frontend_port      = 5432
+        backend_port       = 6432
+        probe_port         = 8008
+        probe_protocol     = "Http"
+        probe_request_path = "/primary"
+      }
+    }
+  }
 }
