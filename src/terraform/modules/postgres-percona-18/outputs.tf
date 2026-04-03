a906664fd062ef4f339b45193cc068bcd9d071a0
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-03 14:56:36 +0300

    fix: remove public IP output from vm_details in Postgres Percona 18 module

--- src/terraform/modules/postgres-percona-18/outputs.tf
+++ src/terraform/modules/postgres-percona-18/outputs.tf
@@ -4,7 +4,6 @@ output "vm_details" {
     for i in range(var.vm_count) :
     azurerm_linux_virtual_machine.vm[i].name => {
       private_ip = azurerm_network_interface.nic[i].private_ip_address
-      public_ip  = try(azurerm_public_ip.pip[i].ip_address, null)
     }
   }
 }
