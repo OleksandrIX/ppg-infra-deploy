573970c1e39cd4e10ee244cb97f9f1c4329ab5f3
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-04 09:27:04 +0300

    feat: add backend configuration for Azure in Terraform providers

--- envs/test-azure/terraform/providers.tf
+++ envs/test-azure/terraform/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-ppg-cluster-dev"
+    storage_account_name = "ppgclusterpgbackrest2"
+    container_name       = "tfstate"
+    key                  = "test-azure/terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
