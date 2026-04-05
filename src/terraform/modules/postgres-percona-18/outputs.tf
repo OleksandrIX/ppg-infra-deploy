8b9c1a9a266970d0f322d35afeabe2a522c4cd5e
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-05 10:52:54 +0300

    refactor: remove unused output for VM details in outputs.tf

--- src/terraform/modules/postgres-percona-18/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_details" {
-  description = "Map of VM names to network details"
-  value = {
-    for i in range(var.cluster_vm.count) :
-    azurerm_linux_virtual_machine.vm[i].name => {
-      private_ip = azurerm_network_interface.nic[i].private_ip_address
-    }
-  }
-}
