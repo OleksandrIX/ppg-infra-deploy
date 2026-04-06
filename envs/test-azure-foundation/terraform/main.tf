7a4025505aedfe04a8d388c2f2fcf050a7d808fb
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-06 09:21:33 +0300

    feat: replace key vault access policy with role assignment for foundation runner

--- envs/test-azure-foundation/terraform/main.tf
+++ envs/test-azure-foundation/terraform/main.tf
@@ -110,10 +110,8 @@ resource "azurerm_key_vault" "kv" {
   sku_name            = var.key_vault_sku
 }
 
-resource "azurerm_key_vault_access_policy" "foundation_runner" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = ["Get", "List", "Set", "Delete", "Purge", "Recover"]
+resource "azurerm_role_assignment" "me_kv_admin" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
