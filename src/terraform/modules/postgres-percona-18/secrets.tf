003d57bd3ce2f938e041cf637b4979ae4042f59d
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-06 14:02:37 +0300

    fix: disable Ansible run on apply and comment out unused password resources

--- src/terraform/modules/postgres-percona-18/secrets.tf
+++ src/terraform/modules/postgres-percona-18/secrets.tf
@@ -1,60 +1,60 @@
-resource "random_password" "psql_superuser_password" {
-  length  = 48
-  special = false
-}
+# resource "random_password" "psql_superuser_password" {
+#   length  = 48
+#   special = false
+# }
 
-resource "random_password" "psql_replication_password" {
-  length  = 48
-  special = false
-}
+# resource "random_password" "psql_replication_password" {
+#   length  = 48
+#   special = false
+# }
 
-resource "random_password" "pgbouncer_auth_password" {
-  length  = 48
-  special = false
-}
+# resource "random_password" "pgbouncer_auth_password" {
+#   length  = 48
+#   special = false
+# }
 
-resource "random_password" "patroni_restapi_password" {
-  length  = 48
-  special = false
-}
+# resource "random_password" "patroni_restapi_password" {
+#   length  = 48
+#   special = false
+# }
 
-resource "random_password" "pgbackrest_cipher_pass" {
-  length  = 48
-  special = false
-}
+# resource "random_password" "pgbackrest_cipher_pass" {
+#   length  = 48
+#   special = false
+# }
 
-resource "azurerm_key_vault_secret" "psql_superuser_password" {
-  name         = "${var.cluster_vm.name_prefix}-psql-superuser-password"
-  value        = random_password.psql_superuser_password.result
-  key_vault_id = var.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "psql_superuser_password" {
+#   name         = "${var.cluster_vm.name_prefix}-psql-superuser-password"
+#   value        = random_password.psql_superuser_password.result
+#   key_vault_id = var.key_vault_id
+# }
 
-resource "azurerm_key_vault_secret" "psql_replication_password" {
-  name         = "${var.cluster_vm.name_prefix}-psql-replication-password"
-  value        = random_password.psql_replication_password.result
-  key_vault_id = var.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "psql_replication_password" {
+#   name         = "${var.cluster_vm.name_prefix}-psql-replication-password"
+#   value        = random_password.psql_replication_password.result
+#   key_vault_id = var.key_vault_id
+# }
 
-resource "azurerm_key_vault_secret" "pgbouncer_auth_password" {
-  name         = "${var.cluster_vm.name_prefix}-pgbouncer-auth-password"
-  value        = random_password.pgbouncer_auth_password.result
-  key_vault_id = var.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "pgbouncer_auth_password" {
+#   name         = "${var.cluster_vm.name_prefix}-pgbouncer-auth-password"
+#   value        = random_password.pgbouncer_auth_password.result
+#   key_vault_id = var.key_vault_id
+# }
 
-resource "azurerm_key_vault_secret" "patroni_restapi_password" {
-  name         = "${var.cluster_vm.name_prefix}-patroni-restapi-password"
-  value        = random_password.patroni_restapi_password.result
-  key_vault_id = var.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "patroni_restapi_password" {
+#   name         = "${var.cluster_vm.name_prefix}-patroni-restapi-password"
+#   value        = random_password.patroni_restapi_password.result
+#   key_vault_id = var.key_vault_id
+# }
 
-resource "azurerm_key_vault_secret" "pgbackrest_cipher_pass" {
-  name         = "${var.cluster_vm.name_prefix}-pgbackrest-cipher-pass"
-  value        = random_password.pgbackrest_cipher_pass.result
-  key_vault_id = var.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "pgbackrest_cipher_pass" {
+#   name         = "${var.cluster_vm.name_prefix}-pgbackrest-cipher-pass"
+#   value        = random_password.pgbackrest_cipher_pass.result
+#   key_vault_id = var.key_vault_id
+# }
 
-resource "azurerm_key_vault_secret" "pgbackrest_azure_key" {
-  name         = "${var.cluster_vm.name_prefix}-pgbackrest-azure-key"
-  value        = var.pgbackrest_azure_key
-  key_vault_id = var.key_vault_id
-}
+# resource "azurerm_key_vault_secret" "pgbackrest_azure_key" {
+#   name         = "${var.cluster_vm.name_prefix}-pgbackrest-azure-key"
+#   value        = var.pgbackrest_azure_key
+#   key_vault_id = var.key_vault_id
+# }
