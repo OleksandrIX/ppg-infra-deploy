0950c3449f396ec6a4f3b99775299460cd19865e
Oleksandr Zahorenko <oleksandr.zahorenko@gmail.com>
2026-04-08 11:16:33 +0300

    refactor: update naming format for VM resources in Terraform configuration

--- envs/test-azure/terraform/locals.tf
+++ envs/test-azure/terraform/locals.tf
@@ -3,7 +3,7 @@ locals {
   ansible_env_dir     = "${path.module}/ansible"
   wrapper_script_path = "${path.module}/../../../scripts/run-create-cluster.sh"
 
-  postgres_percona_cluster_vm_name_prefix = "ppg-${var.environment}-cluster"
-  postgres_percona_ansible_host_name      = "ppg-${var.environment}-ansible-host"
-  postgres_percona_lb_name                = "ppg-${var.environment}-internal-lb"
+  postgres_percona_cluster_vm_name_prefix = "vm-${var.environment}-ppg"
+  postgres_percona_ansible_host_name      = "vm-${var.environment}-ansible"
+  postgres_percona_lb_name                = "lb-${var.environment}-ppg"
 }
